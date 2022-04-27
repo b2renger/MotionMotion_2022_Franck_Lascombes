75a1f0aea7b0c20a18a493a491466cd2c7724b10
b2renger <berenger.recoules@gmail.com>
2022-04-27 10:48:56 +0200

    chenillard

--- boite4/boite4.ino
+++ boite4/boite4.ino
@@ -1,38 +1,70 @@
 // inclure la bibliothèque servo
 #include <Servo.h>
 #include <Adafruit_NeoPixel.h>
+#include <SimplexNoise.h>
 
-#define NUMPIXELS 30
-// 3 
+#define NUMPIXELS 42
+// 3
 
 // créer un objet que l'on appelera myservo que l'on pourra manipuler pour envoyer
 // des informations à notre servo moteur
 Servo myservo1;
 Adafruit_NeoPixel pixels(NUMPIXELS, 6, NEO_GRB + NEO_KHZ800);
 
+SimplexNoise sn;
+double nx;
+double x = 0.0;
+
+double ny;
+double y = 0.0;
+
 
 void setup() {
   pinMode(3, OUTPUT); // préciser que l'on veut utiliser la pin 6 comme une sortie
-  myservo1.attach(3); 
+  myservo1.attach(3);
 
-   pixels.begin();
- 
+  pixels.begin();
+
+
+  // obtenir des valeurs aléatoires au lancement du programme
+  x = analogRead(0) / 10.;
+  y = analogRead(2) / 10.;
+
 
 }
 
 void loop() {
-  myservo1.write(92); // mettre le servo en position de repos
+  myservo1.write(92.5); // mettre le servo en position de repos
+  x += 0.005;
+  y += 0.001;
+
+  long t = millis()% 5000;
+  int index = map(t, 0, 5000, NUMPIXELS, 0);
 
 
-   for(int i=0; i<NUMPIXELS; i++) { // For each pixel...
+  for (int i = 0; i < NUMPIXELS; i++) { // For each pixel...
+
+    // on calcule une valeur de noise (comprise entre 0 et 1)
+    //nx = sn.noise(x, i*.5);
+    // on fait de même avec notre seconde valeur.
+    //ny = sn.noise(y, i * 2);
+
+    //float mult = map(nx * 100, -100, 100, 5, 150) ;
 
     // pixels.Color() takes RGB values, from 0,0,0 up to 255,255,255
     // Here we're using a moderately bright green color:
-    pixels.setPixelColor(i, pixels.Color(180, 180, 180));
-   }
+    
+    if (i > (index-5) && i <= index ) {
+      pixels.setPixelColor(i, pixels.Color(255,255,100));
+    }
+    else {
+     pixels.setPixelColor(i, pixels.Color(0,0,0));
+    }
+    // pixels.setPixelColor(i, pixels.gamma32(pixels.ColorHSV (55, 50, mult)));
+  }
 
-    pixels.show(); 
- 
-  //delay(100);
- 
+  pixels.show();
+
+  delay(5);
+
 }
